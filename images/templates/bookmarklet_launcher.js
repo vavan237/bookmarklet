48f6de7127d893b46cbf9560e89c00a37e613916
vavan <windhover@mail.ru>
2021-09-24 13:12:47 +0300

    bookmarklet_launcher changed

--- images/templates/bookmarklet_launcher.js
+++ images/templates/bookmarklet_launcher.js
@@ -3,6 +3,6 @@
 		myBookmarklet()
 	}
 	else {
-		document.body.appendChild(document.createElement('script')).src='https://3190-85-26-234-182.ngrok.io/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
+		document.body.appendChild(document.createElement('script')).src='https://alpic.herokuapp.com/static/js/bookmarklet.js?r='+Math.floor(Math.random()*99999999999999999999);
 	}
 })();
